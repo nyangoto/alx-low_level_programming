e16abd1afe76a35f129be27fc45ad317142b7852
nyangoto <patricknyangoto@hotmail.com>
2021-02-03 20:59:20 +0000

    Initial commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/** main - program entry point
+ *@void: no arguments
+ *Description: The programs starts and ends here
+ *Return: program exits with 0
+ */
+int main(void)
+{
+  puts ("\"Programming is like a multilingual puzzle");
+  return(0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main is the program entry point
+ *@void: no arguments
+ *Description: The program starts and ends here
+ *Return: program exits with value 0
+ */
+
+int main(void)
+{
+  printf("with prper grammr, but the outcome is a piece of art,\n");
+  return(0);
+}
