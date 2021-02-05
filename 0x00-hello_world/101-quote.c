f67dade82ceeddb50b75238d5d70876a3b7d210e
nyangoto <patricknyangoto@hotmail.com>
2021-02-05 11:20:20 +0000

    Advanced Tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - This function prints out the following string to standard error
+* and that piece of art is useful" - Dora Korpar, 2015-10-19
+* Return: This function returns 1
+*/
+int main (void)
+{
+fprintf(stderr, "and that piece if art is useful\" - Dora Korpar, ");
+fprintf(stderr, "2015-10-19\n";
+return (1);
+}
