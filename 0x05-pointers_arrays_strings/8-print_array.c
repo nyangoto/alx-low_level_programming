e07df9ee1d2e5613793a57844bf7d014a2d53b5a
nyangoto <patricknyangoto@hotmail.com>
2021-02-16 17:29:37 +0000

    Final corrections

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,15 +10,16 @@
 */
 void print_array(int *a, int n)
 {
-int i = 0;
+int i;
+
 for (i = 0; i < n; i++)
 {
-a[i] = *(a + i);
 printf("%d", a[i]);
-if (i != n-1)
+if (i != n - 1)
 {
-printf(", ")
+printf(",");
+printf(" ");
 }
-
 }
+printf("\n");
 }
