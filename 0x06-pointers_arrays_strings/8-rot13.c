d46c0f7f0dd50eb837ce7407644b8934414925fb
nyangoto <patricknyangoto@hotmail.com>
2021-02-19 17:32:39 +0000

    Initial commit

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+* rot13 - encodes a string into rot13
+* @s: string to be encoded
+*
+* Return: encoded string
+*/
+char *rot13(char *s)
+{
+int i = 0, j;
+
+char let[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char let13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqestuvwxyzabcdefghijklm";
+
+while (*(s + i) != '\0')
+{
+for (j = 0; j < 53; j++)
+{
+if (let[j] == s[i])
+{
+s[i] = let13[j];
+break;
+}
+}
+i++;
+}
+return (s);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains projects pertaining low level programming concepts;pointers, strings and arrays written in ANSI C.
