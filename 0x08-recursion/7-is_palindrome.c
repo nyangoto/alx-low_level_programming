1dc00c461225d020e434e1ec9d96717d36c1a539
nyangoto <patricknyangoto@hotmail.com>
2021-02-24 18:14:38 +0000

    Initial commit

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+
+/**
+* _strlen_recursion - returns length of string
+* @s: pointer to string
+*
+* Return: length of a string
+*/
+
+int _strlen_recursion(char *s)
+{
+if (*s)
+{
+return (1 + _strlen_recursion(s + 1));
+}
+return (0);
+}
+
+/**
+* niPalindrome - check if string is palindrome
+* @s: pointer to string
+* @fi: first index
+* @li: last index
+*
+* Return: 0 if not, 1 if true
+*/
+
+int niPalindrome(char *s, int fi, int li)
+{
+if (fi < li && s[fi] == s[li])
+{
+niPalindrome(s, fi + 1, li - 1);
+}
+if (s[fi] != s[li])
+{
+return (0);
+}
+return (1);
+}
+
+/**
+* is_palindrome - checks if string is palindrome or not
+* @s: pointer to string
+*
+* Return: 1 if palindrome otherwise return 0
+*/
+
+int is_palindrome(char *s)
+{
+int fi, li;
+
+fi = 0;
+li = _strlen_recursion(s) - 1;
+
+return (niPalindrome(s, fi, li));
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This directory contains project tasks that cover recursion programming concepts written in ANSI C.
