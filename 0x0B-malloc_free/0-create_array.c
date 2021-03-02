3c06a3ada499921e1d0b4a74a78b0f81f1d05a3a
nyangoto <patricknyangoto@hotmail.com>
2021-03-02 20:58:26 +0000

    fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	arr = malloc(sizeof(char) * (size + 1));
+	arr = malloc(sizeof(char) * size);
 
 	if (arr == NULL)
 		return (NULL);
