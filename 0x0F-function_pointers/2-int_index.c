33d8e319746a0ed92a4c6742d1a80a4fe8074588
nyangoto <patricknyangoto@hotmail.com>
2021-03-10 20:57:28 +0000

    Initial lg

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,30 +8,18 @@
 *
 * Return: index of first element for which the cmp function des not return 0.
 */
-int int_index(int *array, int size, int (*cmp)(itn))
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, res;
 
-	res = -1;
-	if (array && cmp)
+	if (array && size > 0 && cmp)
 	{
-		if (size <= 0)
-		{
-			return (res);
-		}
 		for (i = 0; i < size; i++)
 		{
-			cmp(array[i]);
-			if (cmp(array[i]) > 0)
-			{
-				res = i;
-				break;
-			}
-			if ((cmp(array[i] == (-1)))
-			{
-				return (res);
-			}
+			res = cmp(array[i]);
+			if (res != 0)
+				return (i);
 		}
 	}
-	return (res);
+	return (-1);
 }
