a1731adf29377087758ed66f6ad9b32b38709abd
nyangoto <patricknyangoto@hotmail.com>
2021-03-10 21:44:16 +0000

    Final

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,4 @@
-#include "3-calc.h"
+iinclude "3-calc.h"
 /**
 * op_add - returns sum of two numbers
 * @a: integer.
@@ -56,10 +56,5 @@ int op_div(int a, int b)
 */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
