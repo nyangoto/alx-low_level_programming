98aab4d917db6c24c58f57e44a56aa05a9b125a3
nyangoto <patricknyangoto@hotmail.com>
2021-03-25 20:24:39 +0000

    Header filename correction

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 /* Function prototypes herein */
-int putchar(char c);
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 unsigned int _strlen(const char *s);
 void print_binary(unsigned long int n);
